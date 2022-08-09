9106d7ce2952c08280ecb925131ab05e60f81b0b
Prabhjot Sodhi <sodhiprabhjot23@gmail.com>
2022-08-09 13:50:22 +1200

    completed encryption

--- encryption.py
+++ encryption.py
@@ -4,4 +4,11 @@ import bcrypt
 input_password = str(input("Inputer Password: "))
 encoded_password = input_password.encode("utf-8") # encode the password
 encrypted_password = bcrypt.hashpw(encoded_password, bcrypt.gensalt(10)) # encrypt the password
-print(encrypted_password)
+
+check_password = str(input("check password: ")) # input the password to check
+encoded_check_password = check_password.encode("utf-8") # encode the password
+
+if bcrypt.checkpw(encoded_check_password, encrypted_password):
+    print("login success")
+else:
+    print("incorrect password")
