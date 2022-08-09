52ad12fcefc85110ce7c5a6b2e30c095ce09c003
Prabhjot Sodhi <sodhiprabhjot23@gmail.com>
2022-08-09 13:59:25 +1200

    Format encryption file

--- encryption.py
+++ encryption.py
@@ -1,14 +1,24 @@
 import bcrypt
 
-# Temporary password
-input_password = str(input("Inputer Password: "))
-encoded_password = input_password.encode("utf-8") # encode the password
-encrypted_password = bcrypt.hashpw(encoded_password, bcrypt.gensalt(10)) # encrypt the password
 
-check_password = str(input("check password: ")) # input the password to check
-encoded_check_password = check_password.encode("utf-8") # encode the password
+def sign_up(username, password):
+    append_string = username.join(password)
+    encoded_password = append_string.encode("utf-8") # encode the password
+    encrypted_password = bcrypt.hashpw(encoded_password, bcrypt.gensalt(10)) # encrypt the password
+    return encrypted_password
 
-if bcrypt.checkpw(encoded_check_password, encrypted_password):
-    print("login success")
-else:
-    print("incorrect password")
+def login(username, password):
+    append_string = username.join(password)
+    encoded_check_password = append_string.encode("utf-8") # encode the password
+    if bcrypt.checkpw(encoded_check_password, encrypted_password):
+        print("login success")
+    else:
+        print("incorrect password")
+
+if __name__ == "__main__":
+    input_username = str(input("Input Username: "))
+    input_password = str(input("Input Password: "))
+    check_username = str(input("Check Username: ")) # input the password to check
+    check_password = str(input("Check Password: ")) # input the password to check
+    encrypted_password = sign_up(input_username, input_password) # encrypt the password
+    login(check_username, check_password) # login the password
