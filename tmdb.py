0174abdfb42415e0744656731857d03af3d0a416
Prabhjot Sodhi <sodhiprabhjot23@gmail.com>
2022-08-09 12:18:19 +1200

    Filtered TMDB API

--- tmdb.py
+++ tmdb.py
@@ -1,10 +1,14 @@
 import requests
+import json
 
 api_key = "4a8701d372bc34afefe25162a4b71cff"
-drama_name = "Game of Thrones"
+drama_name = ""
 
 drama_url = f"https://api.themoviedb.org/3/search/tv?api_key=4a8701d372bc34afefe25162a4b71cff&query={drama_name}"
-drama_res = requests.request("GET", drama_url)
-results = drama_res.json()['results']
+drama_response = requests.request("GET", drama_url)
+results = drama_response.json()['results']
 
-print(results)
+filter_results = [x for x in results if x['original_language'] == 'ko']
+final_json = json.dumps(filter_results)
+
+print(final_json)
