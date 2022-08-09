d4964e1acd8bf232b27f1a14f6de53ffa7bfbaea
Prabhjot Sodhi <sodhiprabhjot23@gmail.com>
2022-08-10 11:24:50 +1200

    encryption for registering users

--- encryption.py
+++ encryption.py
@@ -8,4 +8,19 @@ class PasswordDatabase:
                 self.data = pickle.load(f)
         except:
             self.data = {}
+    def register(self, user, password):
+        if user in self.data:
+            return False
+        pwd_hash = self.hash_password(password)
+        self.data[user] = pwd_hash
+        with open('encrypted_dict.json', 'wb') as f:
+            pickle.dump(self.data, f, protocol=pickle.HIGHEST_PROTOCOL)
+        return True
+
 db = PasswordDatabase()
+
+print("-------------Registering users-------------")
+print(db.register('john', '123'))
+print(db.register('john', '123'))
+print(db.register('jeff', '345'))
+print(db.register('joe', '456'))
