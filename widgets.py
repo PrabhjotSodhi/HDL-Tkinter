aa93d0a5b342e73c63b1f3d1be4bc708a84c02e6
Prabhjot Sodhi <sodhiprabhjot23@gmail.com>
2022-08-12 18:50:48 +1200

    Virtual Environment + InputBox widget WIP

--- widgets.py
+++ widgets.py
@@ -1,5 +1,4 @@
 import tkinter as tk
-from turtle import width
 import customtkinter as ctk
 import tkinter.font as font
 
@@ -10,20 +9,19 @@ class WrappingLabel(tk.Label):
         tk.Label.__init__(self, master, **kwargs)
         self.bind('<Configure>', lambda e: self.config(wraplength=self.winfo_width()))
 
-
-
 class Button(ctk.CTkButton):
     '''A custom-styled button class'''
     def __init__(self, master=None, *args, **kwargs):
         ctk.CTkButton.__init__(self, master, text_font=button_font, *args, **kwargs)
-        self.bind(self.configure(fg_color="#48BB78",hover_color="#38A169",text_color="#FFFFFF",corner_radius=0,command=lambda: print("hello")))
+        self.bind(self.configure(fg_color="#48BB78",hover_color="#38A169",text_color="#FFFFFF",command=lambda: print("hello")))
 
-class InputBox(tk.Entry):
+class InputBox(ctk.CTkEntry):
     '''A custom-styled button class'''
-    def __init__(self, master=None, *args, **kwargs):
-        tk.Entry.__init__(self, master, *args, **kwargs)
+    def __init__(self, master=None, placeholder="PLACEHOLDER", *args, **kwargs):
+        ctk.CTkEntry.__init__(self, master,placeholder_text=placeholder,placeholder_text_color="#FFFFFF",border_width=2,corner_radius=0,*args, **kwargs)
         self.input_text = tk.StringVar()
-        self.bind(self.config(bg='#212121',fg="#FFFFFF",font=input_font,relief=tk.FLAT,textvariable=self.input_text))
+        self.bind(self.configure(bg_color='#212121',fg_color="#212121",font=input_font,text_color="#FFFFFF",relief=tk.FLAT,textvariable=self.input_text))
+        
     def get_input(self):
         return self.input_text.get()
 
@@ -34,9 +32,9 @@ root.configure(bg='#212121')
 input_font = font.Font(family='Poppins', size=8, weight='normal')
 button_font = font.Font(family='Poppins', size=8, weight='normal')
 
-my_input = InputBox(root, text='Sign In')
-my_button = Button(root, text='Sign In', width=183*2, height=48, )
+my_input = InputBox(root, placeholder_text="Enter your name", width=183*2, height=48)
+my_button = Button(root, text='Sign In', width=183*2, height=48)
 my_button.place(relx=0.5, rely=0.5, anchor=tk.CENTER)
-my_input.pack(ipadx=10,ipady=10)
+my_input.place(relx=0.5, rely=0.3, anchor=tk.CENTER)
 
 root.mainloop()
