54e2b07cd99c84c7e9b1926deceae63f30b55a6e
Prabhjot Sodhi <sodhiprabhjot23@gmail.com>
2022-08-15 19:04:59 +1200

    DramaCard Init

--- widgets.py
+++ widgets.py
@@ -1,7 +1,10 @@
 import tkinter as tk
 import customtkinter as ctk
 import tkinter.font as font
-
+from PIL import Image, ImageTk
+import urllib.request
+from io import BytesIO
+import ssl
 
 root = ctk.CTk()
 root.geometry('450x576')
@@ -34,14 +37,21 @@ class InputBox(ctk.CTkEntry):
         return self.get()
 
 class DramaCard(ctk.CTkFrame):
-    def __init__(self, master=None, cover=None, title=None, year=None, description=None, genres=None, *args, **kwargs):
+    def __init__(self, master=None, cover_url=None, title=None, year=None, description=None, genres=None, *args, **kwargs):
         ctk.CTkFrame.__init__(self, master, width=366, height=168, bg_color="#333333", *args, **kwargs)
-        cover = ImageTk.PhotoImage(Image.open(cover))
+        #context=ssl._create_unverified_context()
+        raw_cover = urllib.request.urlopen(cover_url).read()
+        image = []
+        cover_img = ImageTk.PhotoImage(Image.open(BytesIO(raw_cover)))
+        self.cover = ctk.CTkLabel(self, image=cover_img)
+        self.cover.place(relx=0.1, rely=0.1, anchor=tk.CENTER)
+        image.append(cover_img)
+
 
 
 my_input = InputBox(root, placeholder_text="Enter your name", width=366, height=48)
 my_button = Button(root, text='Sign In', width=366, height=48, command=lambda: print(my_input.get()))
-my_drama_card = DramaCard(root)
+my_drama_card = DramaCard(root, cover_url="https://image.tmdb.org/t/p/original/dDlEmu3EZ0Pgg93K2SVNLCjCSvE.jpg")
 
 my_button.place(relx=0.5, rely=0.5, anchor=tk.CENTER)
 my_input.place(relx=0.5, rely=0.4, anchor=tk.CENTER)
