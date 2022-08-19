00a8d2c2cab639eb65d6a2f77a9ea20e93a417db
Prabhjot Sodhi <sodhiprabhjot23@gmail.com>
2022-08-19 14:16:14 +1200

    Fixed inconsistent colors of widgets and frames

--- main.py
+++ main.py
@@ -13,6 +13,7 @@ class App(ctk.CTk):
         self.title("HangukDramaList")
         self.geometry("450x576")
         self.protocol("WM_DELETE_WINDOW", self.on_closing)
+        
 
         self.grid_rowconfigure(0, weight="1")
         self.grid_columnconfigure(0, weight="1")
@@ -41,15 +42,15 @@ class LoginScreen(ctk.CTkFrame):
     def __init__(self, parent):
         super().__init__(parent)
 
-        self.configure(bg="#212121")
-        content_frame = ctk.CTkFrame(self, bg="#212121")
+        self.configure(fg_color="#212121")
+        content_frame = ctk.CTkFrame(self, fg_color="#212121")
         content_frame.grid(row=0, column=0, sticky="")
         self.grid_columnconfigure(0, weight=1)
         self.grid_rowconfigure(0, weight=1)
         content_frame.grid_columnconfigure(0, weight=1)
         content_frame.grid_rowconfigure(0, weight=1)
 
-        ctk.CTkLabel(content_frame, text="Welcome Back", text_font=w.FONT_TITLE, text_color="#FFFFFF", anchor="w").grid(sticky="nws", pady=(0,60))
+        ctk.CTkLabel(content_frame, text="Welcome Back", text_font=w.FONT_TITLE, text_color="#FFFFFF", anchor="w").grid(sticky="nws", pady=(29,60))
 
 
         ctk.CTkLabel(content_frame, text="Email", text_font=w.FONT_INPUT, text_color="#FFFFFF", anchor="w").grid(sticky="nws")
@@ -63,8 +64,8 @@ class LoginScreen(ctk.CTkFrame):
         w.Button(content_frame, text="Sign In", width=366, height=48, command=lambda: parent.show_screen(SignupScreen)).grid()
         #ctk.CTkButton(content_frame, text="Sign In", command=lambda: parent.show_screen(SignupScreen)).grid(pady=0,padx=0)
 
-        signup_frame = ctk.CTkFrame(content_frame, bg="#212121")
-        signup_frame.grid(sticky="s", pady=(87,0))
+        signup_frame = ctk.CTkFrame(content_frame, fg_color="#212121")
+        signup_frame.grid(sticky="s", pady=(87,28))
 
         ctk.CTkLabel(signup_frame, text="Don't have an account?", text_font=w.FONT_INPUT, text_color="#FFFFFF", anchor="w").grid(sticky="nws")
         ctk.CTkButton(signup_frame, text="Sign Up", text_font=w.FONT_INPUT, text_color="#48BB78", fg_color="#212121", hover_color="#212121", width=30, command=lambda: parent.show_screen(SignupScreen)).grid(row=0, column=1,pady=0,padx=0)
