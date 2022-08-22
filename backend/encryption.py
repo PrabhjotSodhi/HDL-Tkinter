6e9aab8a7b580c097e3567cb0fc476d738977bc8
Prabhjot Sodhi <sodhiprabhjot23@gmail.com>
2022-08-22 12:57:52 +1200

    Signup screen init + nickname encryption

--- backend/encryption.py
+++ backend/encryption.py
@@ -1,6 +1,18 @@
 import pickle
 import bcrypt
 
+watchlist = {
+    "plan_to_watch":[
+    ],
+    "currently_watching":[
+    ],
+    "completed":[
+    ],
+    "on_hold":[
+    ],
+}
+
+
 class PasswordDatabase:
     def __init__(self):
         try:
@@ -9,11 +21,11 @@ class PasswordDatabase:
         except: # if file does not exist or is empty, create new dictionary
             self.data = {}
 
-    def register(self, user, password):
+    def register(self, nickname, user, password):
         if user in self.data: # if the user already exists in the database, return False
             return False
         pwd_hash = self.hash_password(password) # hash the password
-        self.data[user] = pwd_hash # add the user to the dictionary
+        self.data[user] = [nickname, pwd_hash, watchlist] # add the user to the dictionary
         with open('encrypted_dict.json', 'wb') as f: # save the dictionary to the file
             pickle.dump(self.data, f, protocol=pickle.HIGHEST_PROTOCOL) 
         return True # Use successfully registered
@@ -22,25 +34,30 @@ class PasswordDatabase:
         if user not in self.data: # if the user does not exist in the database, return False
             return False
         pwd_bytes = password.encode('utf-8')
-        return bcrypt.checkpw(pwd_bytes, self.data[user]) # check if the password is correct
+        #if bcrypt.checkpw(pwd_bytes, self.data[user][1]): # if the password is correct, return True
+        #    return self.data[user][0:2]
+        return bcrypt.checkpw(pwd_bytes, self.data[user][1]) # check if the password is correct
 
     def hash_password(self, password):
         pwd_bytes = password.encode("utf-8")
         salt = bcrypt.gensalt() # generate a salt that is used to hash the password
         return bcrypt.hashpw(pwd_bytes, salt)
 
-'''
+
 db = PasswordDatabase()
 
 # Test the system
 print("-------------Registering users-------------")
-print(db.register('john', '123'))
-print(db.register('john', '123'))
-print(db.register('jeff', '345'))
-print(db.register('joe', '456'))
+print(db.register('john','john', '123'))
+print(db.register('john','john', '123'))
+print(db.register('john','jeff', '345'))
+print(db.register('john','joe', '456'))
 
 print("-------------Login-------------")
 print(db.login('john', '123'))
 print(db.login('john', '345'))
 print(db.login('joe', '678'))
-'''
+
+
+
+#{user: [nickname, pass, {currently watching: [],  }}
--- main.py
+++ main.py
@@ -86,23 +86,30 @@ class SignupScreen(ctk.CTkFrame):
         self.configure(fg_color="#212121")
         content_frame = ctk.CTkFrame(self, fg_color="#212121")
         content_frame.grid(row=0, column=0, sticky="")
+
         self.grid_columnconfigure(0, weight=1)
         self.grid_rowconfigure(0, weight=1)
         content_frame.grid_columnconfigure(0, weight=1)
         content_frame.grid_rowconfigure(0, weight=1)
 
-        ctk.CTkLabel(content_frame, text="Create an account", text_font=w.FONT_TITLE, text_color="#FFFFFF", anchor="w").grid(sticky="nws", pady=(29,60))
+        entry_frame = ctk.CTkFrame(content_frame, fg_color="#212121")
+        entry_frame.grid(row=1, column=0, sticky="")
 
+        ctk.CTkLabel(content_frame, text="Create an account", text_font=w.FONT_TITLE, text_color="#FFFFFF", anchor="w").grid(row=0, sticky="nws", pady=(29,0))
 
-        ctk.CTkLabel(content_frame, text="Email", text_font=w.FONT_INPUT, text_color="#FFFFFF", anchor="w").grid(sticky="nws")
-        username = w.InputBox(content_frame, placeholder_text="Enter your email", width=366, height=48)
-        username.grid(pady=(0,27))
+        ctk.CTkLabel(entry_frame, text="Nickname", text_font=w.FONT_INPUT, text_color="#FFFFFF", anchor="w").grid(sticky="nws")
+        username = w.InputBox(entry_frame, placeholder_text="What should we call you?", width=366, height=48)
+        username.grid()
 
-        ctk.CTkLabel(content_frame, text="Password", text_font=w.FONT_INPUT, text_color="#FFFFFF", anchor="w").grid(sticky="nws")
-        password = w.InputBox(content_frame, placeholder_text="Enter your password", width=366, height=48)
-        password.grid(pady=(0,27))
+        ctk.CTkLabel(entry_frame, text="Email", text_font=w.FONT_INPUT, text_color="#FFFFFF", anchor="w").grid(sticky="nws")
+        username = w.InputBox(entry_frame, placeholder_text="Enter your email", width=366, height=48)
+        username.grid()
 
-        w.Button(content_frame, text="Sign In", width=366, height=48, command=lambda: self.sign_in(username, password)).grid()
+        ctk.CTkLabel(entry_frame, text="Password", text_font=w.FONT_INPUT, text_color="#FFFFFF", anchor="w").grid(sticky="nws")
+        password = w.InputBox(entry_frame, placeholder_text="Enter your password", width=366, height=48)
+        password.grid()
+
+        w.Button(content_frame, text="Sign In", width=366, height=48, command=lambda: self.sign_in(username, password)).grid(pady=(27,0))
         #ctk.CTkButton(content_frame, text="Sign In", command=lambda: parent.show_screen(SignupScreen)).grid(pady=0,padx=0)
 
         signup_frame = ctk.CTkFrame(content_frame, fg_color="#212121")
